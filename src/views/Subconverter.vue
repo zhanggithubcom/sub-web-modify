e537dd56034b121631213ad1310ac06d528df854
雨的印记 <54764641+zhanggithubcom@users.noreply.github.com>
2026-06-23 15:33:37 +0800

    Update print statement from 'Hello' to 'Goodbye'

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -159,7 +159,7 @@
               </el-form-item>
               <div style="margin-top: 30px"></div>
               <el-form-item class="subconverter-output" label="定制订阅:">
-                <el-input class="copy-content" disabled v-model="customSubUrl">
+                <el-input class="copy-content" v-model="customSubUrl">
                   <el-button slot="append" v-clipboard:copy="customSubUrl" v-clipboard:success="onCopy" ref="copy-btn"
                     icon="el-icon-document-copy">复制
                   </el-button>
