44ad26b8178ae9eb643c1e782d734ea0bb9fbe0e
雨的印记 <54764641+zhanggithubcom@users.noreply.github.com>
2026-09-03 00:24:54 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -349,7 +349,7 @@ export default {
           "dlj.tf": "https://dlj.tf/short",
         },
         customBackend: {
-          "自建后端": "https://tcc3.cc.cd",
+          "自建后端": "https://tcc3.cc.cd:25500",
           "CM提供-负载均衡后端": "https://subapi.cmliussss.net",
           "CM提供-应急备用后端": "https://subapi.fxxk.dedyn.io",
           "肥羊提供-增强型后端": "https://url.v1.mk",
